75e4988711b9a792420e5f2f919f3194eba58716
cpazad <cpazad@gmail.com>
2023-09-26 13:50:22 +0600

    React toastify added and working on click

--- src/Pages/DonationDetails/DonationDetails.jsx
+++ src/Pages/DonationDetails/DonationDetails.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useLoaderData, useParams } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const DonationDetails = () => {
   const donations = useLoaderData();
@@ -7,19 +9,37 @@ const DonationDetails = () => {
   const idInt = parseInt(id);
   const donation = donations.find((donation) => donation.id === idInt);
   const {description, price, picture, title, category_bg} = donation;
+  
+  // toast function
+  const showToast = (message) => {
+    toast.success(message, { autoClose: 2000 }); 
+  };
+  
+  const handleAddDonation = () => {
+    return showToast('Donation added succeefully, Thank You');
+  }
+
+ 
+  
   return (
     <div>
          <div className="container">
         <img className="w-full rounded-md" src={picture} alt="Image" />
         <div className="content text-left">
           
-            <button style={{backgroundColor:category_bg}} className="btn text-white border-0 capitalize">Donate {price}$</button>
+            <button
+            onClick={handleAddDonation} 
+            style={{backgroundColor:category_bg}} 
+            className="btn text-white border-0 capitalize">
+            Donate {price}$
+            </button>
         </div>
       </div>
       <div className="my-10 text-left p-5">
         <h2 className="text-3xl font-bold mb-3"> {title} </h2>
         <p>{description}</p>
       </div>
+      <ToastContainer />
     </div>
   )
 }
